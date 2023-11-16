780887d4ba9450cdbd60047c8cf7b5444873b6f3
krenemy <aaryaveer.bhardwaj@gmail.com>
2023-11-16 16:34:33 +0530

    errhandling

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 
+
+app.use(morgan("dev"));
+
+const productRoutes = require("./api/routes/products") 
+const orderRoutes = require("./api/routes/orders")
+
+app.use("/products",productRoutes)
+app.use("/orders",orderRoutes)
+
+app.use((req,res,next)=>{
+    const error = new Error ("Not found")
+    error.status(400)
+    next(error)
+})
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
+    res.status(error.staus || 400);
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
